3d9390d35fb698c9d77493e894dde002b9eec157
Orkhan Gasanov <gasanov.orkhan1@gmail.com>
2021-05-14 11:38:51 -0700

    Week 5: Find the Minimum Number of Fibonacci Numbers Whose Sum Is K solved

---
+++ Week15/FindTheMinimumNumberOfFibonacciNumbersWhoseSumIsK.java
@@ -0,0 +1,22 @@
+class FindTheMinimumNumberOfFibonacciNumbersWhoseSumIsK {
+    public int findMinFibonacciNumbers(int k) {
+        int prev=1;
+        int current=1;
+        List<Integer> list = new ArrayList();
+        list.add(prev);
+        while (current<=k) {
+            list.add (current);
+            int temp = current;
+            current+=prev;
+            prev=temp;
+       }
+       int result=0;
+       for (int i=list.size()-1; i>0; i--) {
+          if (list.get(i)<=k) {
+            k-=list.get(i);
+            result++;
+          }
+      }
+    return result;
+    }
+}
