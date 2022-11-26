1b7f4f1c2c0572973c4ce2b094e0b657b2806a5d
Orkhan Gasanov <gasanov.orkhan1@gmail.com>
2022-11-25 20:39:19 -0800

    Reverse Linked List II solved

---
+++ Week5/ReverseLinkedListTwo.java
@@ -0,0 +1,29 @@
+public class ReverseLinkedListTwo {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = head;
+        ListNode prev = dummy;
+        int i = 1;
+        while (i < left) {
+            cur = cur.next;
+            prev = prev.next;
+            i++;
+        }
+        ListNode start = prev;
+        ListNode end = cur;
+        ListNode tmp = null;
+        while (i <= right) {
+            tmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmp;
+            i++;
+        }
+        start.next = prev;
+        end.next = tmp;
+        return dummy.next;
+    }
+
+}
