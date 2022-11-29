0f4a9d36843987439a7b21f8f688626b0735bce1
Orkhan Gasanov <gasanov.orkhan1@gmail.com>
2022-11-29 01:10:33 -0800

    Implement Trie (Prefix Tree) solved

---
+++ Week5/Trie.java
@@ -0,0 +1,54 @@
+public class Trie {
+
+    public Trie() {
+        root = new Node('/');
+    }
+
+    private final Node root;
+
+    public void insert(String word) {
+        Node current = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            int child = word.charAt(i) - 'a';
+            if (current.children[child] == null) {
+                current.children[child] = new Node(word.charAt(i));
+            }
+            current = current.children[child];
+        }
+        current.isComplete = true;
+    }
+
+    public boolean search(String word) {
+        Node current = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            int child = word.charAt(i) - 'a';
+            if (current.children[child] == null) {
+                return false;
+            }
+            current = current.children[child];
+        }
+        return current.isComplete;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node current = this.root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int child = prefix.charAt(i) - 'a';
+            if (current.children[child] == null) {
+                return false;
+            }
+            current = current.children[child];
+        }
+        return true;
+    }
+
+    private static class Node {
+        private boolean isComplete;
+        private final Node[] children;
+
+        public Node(char data) {
+            this.isComplete = false;
+            this.children = new Node[26];
+        }
+    }
+}
