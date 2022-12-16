d952f9d0ffe64708a80e525a18c807021691d34a
Orkhan Gasanov <gasanov.orkhan1@gmail.com>
2022-12-16 00:45:45 -0800

    Longest String Chain solved

---
+++ Week6/LongestStringChain.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestStringChain {
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        int result = 0;
+        Map<String, Integer> dp = new HashMap<>();
+
+        for (String word : words) {
+            dp.put(word, 1);
+            for (int i = 0; i < word.length(); i++) {
+                StringBuilder current = new StringBuilder(word);
+                String next = current.deleteCharAt(i).toString();
+                if (dp.containsKey(next)) {
+                    dp.put(word, Math.max(dp.get(word), dp.get(next) + 1));
+                }
+            }
+            result = Math.max(result, dp.get(word));
+        }
+
+        return result;
+    }
+}
