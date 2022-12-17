9f1cebf62422445e61a96429b284bfa3f2f83231
Orkhan Gasanov <gasanov.orkhan1@gmail.com>
2022-12-17 06:59:40 -0800

    Maximize Sum Of Array After K Negations solved

--- Week7/MaximizeSumOfArrayAfterKNegations.java
+++ Week7/MaximizeSumOfArrayAfterKNegations.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -7,17 +6,17 @@ public class MaximizeSumOfArrayAfterKNegations {
     public int largestSumAfterKNegations(int[] A, int K) {
         Queue<Integer> minHeap = new PriorityQueue<>();
 
-        for(int a: A){
+        for (int a : A) {
             minHeap.add(a);
         }
 
-        for(int i=0 ; i<K ; ++i){
+        for (int i = 0; i < K; ++i) {
             int curr = minHeap.poll();
-            minHeap.add(-1*curr);
+            minHeap.add(-1 * curr);
         }
 
         int result = 0;
-        while(!minHeap.isEmpty()){
+        while (!minHeap.isEmpty()) {
             result += minHeap.poll();
         }
 
