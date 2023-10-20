77bbc5a2ac5d68cd61521f2b201789a5db1422d9
Orkhan <gasanov.orkhan1@gmail.com>
2023-10-19 20:02:23 -0700

    Week 17 completed

--- Week17/AllPathsFromSourceToTarget.java
+++ Week17/AllPathsFromSourceToTarget.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 public class AllPathsFromSourceToTarget {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List <List<Integer>> results = new ArrayList <> ();
+        List<List<Integer>> results = new ArrayList<>();
 
-        Queue<List<Integer>> queue = new LinkedList <> ();
-        queue.add (Arrays.asList (0));
+        Queue<List<Integer>> queue = new LinkedList<>();
+        queue.add(Arrays.asList(0));
 
-        while (!queue.isEmpty ()) {
+        while (!queue.isEmpty()) {
             List<Integer> current = queue.poll();
 
-            if (current.get(current.size ()-1)==graph.length-1) {
-                results.add (current);
+            if (current.get(current.size() - 1) == graph.length - 1) {
+                results.add(current);
             }
 
-            for (int i: graph [current.get(current.size ()-1)]) {
-                List <Integer> result = new ArrayList (current);
+            for (int i : graph[current.get(current.size() - 1)]) {
+                List<Integer> result = new ArrayList(current);
                 result.add(i);
                 queue.add(result);
             }
--- Week17/SubarraySumEqualsK.java
+++ Week17/SubarraySumEqualsK.java
@@ -7,14 +7,14 @@ public class SubarraySumEqualsK {
         int count = 0;
         int totalSum = 0;
 
-        Map <Integer, Integer> map = new HashMap ();
-        map.put (0,1);
-        for (int i=0; i<nums.length; i++) {
-            totalSum+=nums[i];
-            if (map.containsKey (totalSum-k)) {
-                count += map.get (totalSum-k);
+        Map<Integer, Integer> map = new HashMap();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+            if (map.containsKey(totalSum - k)) {
+                count += map.get(totalSum - k);
             }
-            map.put (totalSum, map.getOrDefault(totalSum, 0)+1);
+            map.put(totalSum, map.getOrDefault(totalSum, 0) + 1);
         }
         return count;
     }
