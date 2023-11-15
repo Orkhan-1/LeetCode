d932059098711cd3bb4afa6e7423f22dd6e91818
Orkhan <gasanov.orkhan1@gmail.com>
2023-11-14 21:35:56 -0800

    Find Bottom Left Tree Value solved

--- Week19/FindBottomLeftTreeValue.java
+++ Week19/FindBottomLeftTreeValue.java
@@ -1,26 +1,29 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+// Time complexity - O (N)
+// Space complexity - O (N)
 public class FindBottomLeftTreeValue {
 
-    // Time complexity - O (N)
-    // Space complexity - O (N)
     public int findBottomLeftValue(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int result = 0;
+        queue.add(root);
+
+        int result = 0;   //5
+
         while (!queue.isEmpty()) {
             int size = queue.size();
+
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
                 if (i == 0) {
                     result = node.val;
                 }
                 if (node.left != null) {
-                    queue.offer(node.left);
+                    queue.add(node.left);
                 }
-                if (node.right != null){
-                    queue.offer(node.right);
+                if (node.right != null) {
+                    queue.add(node.right);
                 }
             }
         }
