ab1a18051781756e46e4cdc368a72866770a58ce
Orkhan <gasanov.orkhan1@gmail.com>
2023-11-23 20:10:41 -0800

    BinarySearchTreeToGreaterSumTree solved

---
+++ Week20/BinarySearchTreeToGreaterSumTree.java
@@ -0,0 +1,15 @@
+public class BinarySearchTreeToGreaterSumTree {
+
+    int sum = 0;
+
+    public TreeNode bstToGst(TreeNode node) {
+        if (node == null) {
+            return node;
+        }
+        bstToGst(node.right);
+        sum += node.val;
+        node.val = sum;
+        bstToGst(node.left);
+        return node;
+    }
+}
