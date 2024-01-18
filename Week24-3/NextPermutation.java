15dff9ed74ec9bee99c28f8d2081b814f543b0bb
Orkhan <gasanov.orkhan1@gmail.com>
2024-01-17 17:31:50 -0800

    NextPermutation solved

---
+++ Week24-3/NextPermutation.java
@@ -0,0 +1,36 @@
+
+// Time complexity - O (N)
+// Space complexity - O (1)
+public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
