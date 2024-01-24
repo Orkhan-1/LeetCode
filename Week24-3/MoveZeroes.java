32b6fb5183c0a0daf11ca167c402f34688b3ddf2
Orkhan <gasanov.orkhan1@gmail.com>
2024-01-23 20:00:24 -0800

    FindPivotIndex, MoveZeroes  solved

---
+++ Week24-3/MoveZeroes.java
@@ -0,0 +1,20 @@
+
+// Time complexity -  O (N)
+// Space complexity - O (1)
+public class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
