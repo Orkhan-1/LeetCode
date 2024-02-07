3a6ff4f9d12858c073d366a87ffb94b958817bc8
Orkhan <gasanov.orkhan1@gmail.com>
2024-02-06 21:44:28 -0800

    MergeKSortedLists solved

--- 2024/MergeKSortedLists.java
+++ 2024/MergeKSortedLists.java
@@ -2,6 +2,8 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+// Time complexity -  O(N * log(k))
+// Space complexity - O(k) - determined by the PriorityQueue
 public class MergeKSortedLists {
 
     public ListNode mergeKLists(ListNode[] lists) {
@@ -17,19 +19,19 @@ public class MergeKSortedLists {
             }
         }
 
-        ListNode helper = new ListNode(0);
-        ListNode current = helper;
+        ListNode result = new ListNode(0);
+        ListNode current = result;
 
         while (!minHeap.isEmpty()) {
-            ListNode smallest = minHeap.poll();
-            current.next = smallest;
+            ListNode min = minHeap.poll();
+            current.next = min;
             current = current.next;
 
-            if (smallest.next != null) {
-                minHeap.add(smallest.next);
+            if (min.next != null) {
+                minHeap.add(min.next);
             }
         }
 
-        return helper.next;
+        return result.next;
     }
 }
