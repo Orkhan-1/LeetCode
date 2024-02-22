b95c1ee5a23cfd5ac4bce67ff41cbfc4bda001d6
Orkhan <gasanov.orkhan1@gmail.com>
2024-02-21 22:38:25 -0800

    MergeSortedArray solved

---
+++ 2024/MergeSortedArray.java
@@ -0,0 +1,28 @@
+
+// Time complexity - O (N)
+// Space complexity - O (1)
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = nums1.length - 1;
+        int i = m - 1;
+        int j = n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+}
