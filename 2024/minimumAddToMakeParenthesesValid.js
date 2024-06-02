9f1b03af06150f2ec3185a8f43da8c38db8a0c9d
Orkhan <gasanov.orkhan1@gmail.com>
2024-06-01 23:55:01 -0700

    [JS] MinimumAddToMakeParenthesesValid solved

---
+++ 2024/minimumAddToMakeParenthesesValid.js
@@ -0,0 +1,22 @@
+//            JS
+//     Time complexity - O (N)
+//     Space complexity - O (1)
+
+var minAddToMakeValid = function minAddToMakeValid(s) {
+    let openParentheses = 0;
+    let minAdd = 0;
+
+    for (let char of s) {
+        if (char === '(') {
+            openParentheses++;
+        } else if (char === ')') {
+            if (openParentheses === 0) {
+                minAdd++;
+            } else {
+                openParentheses--;
+            }
+        }
+    }
+
+    return minAdd + openParentheses;
+};
