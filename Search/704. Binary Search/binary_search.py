8da4fd245978dd60926b636dfc94253010fa5621
Orkhan <gasanov.orkhan1@gmail.com>
2024-06-03 22:56:43 -0700

    [Java, Python] BinarySearch solved

---
+++ Search/704. Binary Search/binary_search.py
@@ -0,0 +1,19 @@
+#     Python
+# Time complexity - O (logN)
+# Space complexity - O (1)
+class Solution:
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
