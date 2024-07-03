4331bde43c78e3944098932b7e2632f7f8162da2
Orkhan <gasanov.orkhan1@gmail.com>
2024-07-03 03:59:44 -0700

    [Java] 516. Longest Palindromic Subsequence solved

---
+++ java/00516_longest_palindromic_subsequence.java
@@ -0,0 +1,28 @@
+class Test {
+
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        if (n == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
