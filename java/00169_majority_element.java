607004eead3506691514df65f0af519459dd64c3
Orkhan <gasanov.orkhan1@gmail.com>
2024-09-02 21:40:39 -0700

    169. Majority Element solved

---
+++ java/00169_majority_element.java
@@ -0,0 +1,19 @@
+//        Java
+// Time complexity - O (N)
+// Space complexity - O (1)
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
---
+++ python/00169_majority_element.py
@@ -0,0 +1,15 @@
+
+#        Python
+#  Time complexity - O (N)
+#  Space complexity - O (1)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+
+        return candidate
