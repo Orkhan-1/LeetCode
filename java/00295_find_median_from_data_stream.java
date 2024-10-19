adfe14c07d815e81f5e742cadd6caf8591c20db2
Orkhan <gasanov.orkhan1@gmail.com>
2024-10-18 23:07:36 -0700

    Refactoring

--- java/00295_find_median_from_data_stream.java
+++ java/00295_find_median_from_data_stream.java
@@ -1,5 +1,3 @@
-import java.util.PriorityQueue;
-
 // Time complexity - addNum: O(log n) heap operations, findMedian: O(1)
 // Space complexity - O (N)
 class MedianFinder {
@@ -11,13 +9,18 @@ class MedianFinder {
         minHeap = new PriorityQueue<>();
     }
 
+    // 1,2,3,4,5
+    // Max Heap - smaller half: 1,2,3
+    // Min Heap - larger half: 4,5
     public void addNum(int num) {
         maxHeap.offer(num);
 
         if (!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
-            minHeap.offer(maxHeap.poll());
+            minHeap.offer(maxHeap.poll()); //Max-Heap: 1,2,4 //Min-Heap: 3
+            //Max-Heap: 1,2 //Min-Heap: 3,4
         }
 
+        // balance
         if (maxHeap.size() > minHeap.size() + 1) {
             minHeap.offer(maxHeap.poll());
         } else if (minHeap.size() > maxHeap.size()) {
--- python/00295_find_median_from_data_stream.py
+++ python/00295_find_median_from_data_stream.py
@@ -7,13 +7,17 @@ class MedianFinder:
         self.max_heap = []
         self.min_heap = []
 
+     # 1,2,3,4,5
+     # Max Heap - smaller half: 1,2,3
+     # Min Heap - larger half: 4,5
     def addNum(self, num: int) -> None:
         heapq.heappush(self.max_heap, -num)
 
         if (self.max_heap and self.min_heap and
                 (-self.max_heap[0] > self.min_heap[0])):
-            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
-
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))  # Max-Heap: 1,2,4 //Min-Heap: 3
+                                                                          # Max-Heap: 1,2 //Min-Heap: 3,4
+        # balance
         if len(self.max_heap) > len(self.min_heap) + 1:
             heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
         elif len(self.min_heap) > len(self.max_heap):
