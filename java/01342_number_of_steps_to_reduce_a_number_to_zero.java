2d94b4b7e08a794b51595cec383b36c1fca6b2dc
Orkhan <gasanov.orkhan1@gmail.com>
2024-11-05 00:54:44 -0800

    1342. Number of Steps to Reduce a Number to Zero solved

---
+++ java/01342_number_of_steps_to_reduce_a_number_to_zero.java
@@ -0,0 +1,31 @@
+/*
+n=14
+if even /2
+if odd -1
+
+14 => 1110
+if even /2 [Check the least significant bit (0)  => righshift 1110 > 0111]
+if odd -1  [Check the least significant bit (1)  => flip 0111 > 0110]
+
+
+Time complexity - O(log n)
+Space complexity - O(1)
+
+*/
+
+class Solution {
+
+    public int numberOfSteps(int n) {
+        int steps = 0;
+        while (n != 0) {
+            if ((n & 1) == 0) {
+                n >>= 1;
+            } else {
+                n = n & ~1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+
+}
---
+++ python/01342_number_of_steps_to_reduce_a_number_to_zero.py
@@ -0,0 +1,23 @@
+# n=14
+ # if even /2
+ # if odd -1
+
+ # 14 => 1110
+ # if even /2 [Check the least significant bit (0)  => righshift 1110 > 0111]
+ # if odd -1  [Check the least significant bit (1)  => flip 0111 > 0110]
+
+
+# Time complexity - O(log n)
+# Space complexity - O(1)
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        steps = 0
+
+        while num != 0:
+            if (num & 1) == 0:
+                num >>= 1
+            else:
+                num &= ~1
+            steps += 1
+
+        return steps
