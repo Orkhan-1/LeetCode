8f564e1d0b79a78c484a94c5a22ba4b87a5eba19
Orkhan <gasanov.orkhan1@gmail.com>
2024-11-30 16:39:30 -0800

    1140. Stone Game II solved

---
+++ python/01140_stone_game_II.py
@@ -0,0 +1,29 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        def getMaxScore(turn: bool, m: int, index: int, memo: dict) -> int:
+            if index >= len(piles):
+                return 0
+
+            key = (index, m, turn)
+            if key in memo:
+                return memo[key]
+
+            maxScore = float('-inf') if turn else float('inf')
+            tempScore = 0
+
+            for x in range(1, 2 * m + 1):
+                if index + x > len(piles):
+                    break
+
+                tempScore += piles[index + x - 1]
+
+                if turn:
+                    maxScore = max(maxScore, tempScore + getMaxScore(False, max(m, x), index + x, memo))
+                else:
+                    maxScore = min(maxScore, getMaxScore(True, max(m, x), index + x, memo))
+
+            memo[key] = maxScore
+            return maxScore
+
+        memo = {}
+        return getMaxScore(True, 1, 0, memo)
