a64107a609ae97ee552d75e8abdd85ae9a399d9c
Orkhan <gasanov.orkhan1@gmail.com>
2024-12-14 02:41:27 -0800

    198. House Robber solved

---
+++ python/00198_house_robber.py
@@ -0,0 +1,22 @@
+#       Python
+# Time complexity - O (N)
+# Space complexity - O (N)
+
+# example: nums = [1,2,3,1]
+
+class Solution:
+    def rob(self, nums):
+
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        return dp[-1]
