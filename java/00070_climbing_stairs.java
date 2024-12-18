02ccc90c63ac59ed1ffc551eb5ad63f42755d370
Orkhan <gasanov.orkhan1@gmail.com>
2024-12-18 00:20:02 -0800

    Climbing Stairs solved

--- java/00070_climbing_stairs.java
+++ java/00070_climbing_stairs.java
@@ -1,8 +1,10 @@
-public class ClimbingStairs {
+// Time complexity - O (N)
+// Space complexity - O (N)
 
+class Solution {
     public int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+        if (n <= 2) {
+            return n;
         }
 
         int[] dp = new int[n + 1];
@@ -12,6 +14,7 @@ public class ClimbingStairs {
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
+
         return dp[n];
     }
 }
---
+++ python/00070_climbing_stairs.py
@@ -0,0 +1,19 @@
+# Time complexity - O (N)
+# Space complexity - O (N)
+
+# 4
+# 1 2 3 5
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
